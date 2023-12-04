3f54ba62e138db75f5d9169d8491f2d18c389604
Jérôme <jerome.coeck@outlook.com>
2023-12-04 14:28:27 +0100

    user info retrieval

--- server/routes/user.mjs
+++ server/routes/user.mjs
@@ -42,4 +42,20 @@ userRoutes.post("", async (req, res) => {
     
 });
 
+userRoutes.get("", async (req, res) => {
+    try {
+        const userId = req.session.user.userId;
+
+        const user = await User.findById(userId);
+
+        if (user) {
+            res.send({user});
+        } else {
+            throw new Error('User not found');
+        }
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+});
+
 export default userRoutes;
