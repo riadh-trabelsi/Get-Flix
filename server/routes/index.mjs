a5d5f848e97ef69eda5e8f62f03900d63af8ae68
Jérôme <jerome.coeck@outlook.com>
2023-12-04 11:04:17 +0100

    fix serve

--- server/routes/index.mjs
+++ server/routes/index.mjs
@@ -1,21 +1,11 @@
 import userRoutes from './user.mjs'
-import express from 'express'
 import movieRoutes from './movie.mjs'
-import cors from 'cors'
 
-
-const app = express()
-
-app.use(cors()) // Enable CORS for all routes
 
 // Other middleware and configurations...
 
-app.use('/api/movies', movieRoutes)
 
 // Other routes and configurations...
 
-const port = process.env.PORT || 5050
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`)
-})
+
 export { userRoutes, movieRoutes }
