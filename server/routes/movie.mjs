c67b25cb93f71f1157b3c3843d808ffd60716673
Yves Bolong <yve17@hotmail.be>
2023-12-05 15:48:27 +0100

    movie filters

--- server/routes/movie.mjs
+++ server/routes/movie.mjs
@@ -1,67 +1,70 @@
 import express from 'express'
 import axios from 'axios'
 
+import { filterByGenres, filterByYear } from './movieFilter.mjs'
+
 const movieRoutes = express.Router()
 
 const apiKey = '1cc614b6cd01c73622141ccf0bdceac5'
-const access_token =
-  'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxY2M2MTRiNmNkMDFjNzM2MjIxNDFjY2YwYmRjZWFjNSIsInN1YiI6IjY1NjQ5MWJhYTZjMTA0MDEzODJiMGZlOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.pLiucY7Ytlm6fNHo2cRqaDEdJMoCG7dD42qJCgqcOwI'
 
-// Route for fetching comedy movies
-movieRoutes.get('/comedy', async (req, res) => {
+// Route for fetching upcoming movies, popular movies, latest movies, comedy movies, and action movies
+movieRoutes.get('/home', async (req, res) => {
   try {
-    const response = await axios.get(
-      `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=35`,
-    )
-    const comedyMovies = response.data.results
-    res.json(comedyMovies)
-  } catch (error) {
-    console.error(error)
-    res.status(500).json({ error: 'Internal Server Error' })
-  }
-})
-
-// Route for fetching action movies
-movieRoutes.get('/action', async (req, res) => {
-  try {
-    const response = await axios.get(
-      `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=28`,
-    )
-    const actionMovies = response.data.results
-    res.json(actionMovies)
-  } catch (error) {
-    console.error(error)
-    res.status(500).json({ error: 'Internal Server Error' })
-  }
-})
-
-// Route for fetching latest movies
-movieRoutes.get('/latest', async (req, res) => {
-  try {
-    const response = await axios.get(
-      `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}`,
-    )
-    const latestMovies = response.data.results
-    res.json(latestMovies)
-  } catch (error) {
-    console.error(error)
-    res.status(500).json({ error: 'Internal Server Error' })
-  }
-})
-
-// Route for fetching upcoming movies
-movieRoutes.get('/upcoming', async (req, res) => {
-  try {
-    const response = await axios.get(
+    const upcomingResponse = await axios.get(
       `https://api.themoviedb.org/3/movie/upcoming?api_key=${apiKey}`,
     )
-    const upcomingMovies = response.data.results
-    res.json(upcomingMovies)
+    const popularResponse = await axios.get(
+      `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`,
+    )
+    const latestResponse = await axios.get(
+      `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}`,
+    )
+    const comedyResponse = await axios.get(
+      `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=35`,
+    )
+    const actionResponse = await axios.get(
+      `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=28`,
+    )
+
+    const upcomingMovies = upcomingResponse.data.results
+    const popularMovies = popularResponse.data.results
+    const latestMovies = latestResponse.data.results
+    const comedyMovies = comedyResponse.data.results
+    const actionMovies = actionResponse.data.results
+
+    res.json({
+      upcomingMovies,
+      popularMovies,
+      latestMovies,
+      comedyMovies,
+      actionMovies,
+    })
   } catch (error) {
     console.error(error)
     res.status(500).json({ error: 'Internal Server Error' })
   }
 })
 
-// Exporting the movieRoutes as a module
+// Route for fetching movies by genre
+movieRoutes.get('/genre/:genreId', async (req, res) => {
+  const { genreId } = req.params
+  try {
+    const movies = await filterByGenres(genreId)
+    res.json(movies)
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+})
+
+// Route for fetching movies by year
+movieRoutes.get('/year/:year', async (req, res) => {
+  const { year } = req.params
+  try {
+    const movies = await filterByYear(year)
+    res.json(movies)
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+})
+
 export default movieRoutes
--- server/server.mjs
+++ server/server.mjs
@@ -2,8 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import './db/conn.mjs'
-import { userRoutes, movieRoutes } from './routes/index.mjs';
-
+import { userRoutes, movieRoutes } from './routes/index.mjs'
 
 const PORT = process.env.PORT || 5050
 const app = express()
@@ -11,11 +10,10 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-const apiRouter = express.Router();
-app.use('/api', apiRouter);
-apiRouter.use('/users', userRoutes);
-app.use('/api/movies', movieRoutes);
-
+const apiRouter = express.Router()
+app.use('/api', apiRouter)
+apiRouter.use('/users', userRoutes)
+app.use('/api', movieRoutes)
 
 // start the Express server
 app.listen(PORT, () => {
