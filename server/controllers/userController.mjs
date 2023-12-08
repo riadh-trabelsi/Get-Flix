72a770fe5f971c8615faa767c61df5af8ea58312
Jérôme <jerome.coeck@outlook.com>
2023-12-08 17:26:20 +0100

    signup + getinfo

--- server/controllers/userController.mjs
+++ server/controllers/userController.mjs
@@ -1,70 +1,50 @@
 import User from '../models/userModel.mjs'
-import { signInValidation, signUpValidation } from '../validations/userValidation.mjs'
-import { parseError } from '../util/helpers.mjs'
-import { createSecretToken } from '../util/secretToken.mjs';
+import { signUpValidation } from '../validations/userValidation.mjs'
+import { createAccessToken } from '../util/secretToken.mjs';
+import  responseHandler  from '../util/handlers/responseHandlers.mjs'
 
-export const signUp = async (req, res) => {
+const signup = async (req, res) => {
     try {
-        const {
-        firstname,
-        lastname,
-        // age,
-        email,
-        // username,
-        password,
-        } = req.body;
+        const { firstname, lastname, email, password } = req.body;
 
-        await signUpValidation.validateAsync({
-        firstname,
-        lastname,
-        // age,
-        email,
-        // username,
-        password,
-        });
+        await signUpValidation.validateAsync({firstname, lastname, email, password });
 
-        const newUser = new User({
-        firstname,
-        lastname,
-        // age,
-        email,
-        // username,
-        password,
-        });
+        const newUser = new User({ firstname, lastname, email, password, role: 'registrant' });
        
         await newUser.save();
 
-        res.send(newUser);
-    } catch (err) {
-        res.status(400).send(parseError(err));
+        const accessToken = createAccessToken(newUser);
+
+        res.cookie('jwt', accessToken, {
+            sameSite: "None",
+            withCredentials: true,
+            httpOnly: false, //for dev purposes change to true for prod
+            maxAge: 2 * 60 * 60 * 1000 //cookie expiry: set to match aT (2Hours)
+        });
+
+        responseHandler.created(res, {
+            accessToken,
+            ...newUser._doc,
+            id: newUser.id
+        });
+
+    } catch {
+        responseHandler.error(res);
+        //res.status(400).send(parseError(err)); dev purposes
     };
 };
 
-export const login = async (req, res) => {
-    try{
-        const {
-            email,
-            password,
-        } = req.body;
-
-        await signInValidation.validateAsync({
-            email,
-            password,
-        }) ;
-        
-        const user = await User.findOne({ email });
-        if (user && user.comparePasswords(password)) {
-            const token = createSecretToken(user._id);
-            res.cookie("token", token, {
-                withCredentials: true,
-                httpOnly: false,
-            });
-            res.status(201).json({ message: "User logged in successfully", success: true });
-          
-        } else {
-            throw new Error('Invalid login credentials');
-        }
-    } catch (err) { 
-        res.status(401).send(parseError(err));
-    };
+const getUserInfo = (req,res) => {
+    const user = req.user;
+    if (!user){
+        responseHandler.notFound(res);
+    }
+    responseHandler.ok(res, {
+        ...user._doc,
+    });
 };
+
+export {
+    signup,
+    getUserInfo
+}
