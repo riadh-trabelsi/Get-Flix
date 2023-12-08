81b13df2e24221d23f2b69f0efdbe99fdf3316ee
Jérôme <jerome.coeck@outlook.com>
2023-12-08 17:39:48 +0100

    delete session + visitor files

--- server/routes/index.mjs
+++ server/routes/index.mjs
@@ -1,11 +1,8 @@
 import userRoutes from './userRoute.mjs';
 import authRoutes from './authRoutes.mjs';
-import sessionRoutes from './session.mjs';
-import visitorRoutes from './visitor.mjs';
 import passwordRoutes from './password.mjs';
 import movieRoutes from './movie.mjs'
 
 // import commentRoutes from './comment.mjs';
 
-
-export { userRoutes, sessionRoutes, visitorRoutes, passwordRoutes, movieRoutes, authRoutes /**commentRoutes**/ };
+export { userRoutes, passwordRoutes, movieRoutes, authRoutes /**commentRoutes**/ };
--- server/server.mjs
+++ server/server.mjs
@@ -1,11 +1,9 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
-import session from "express-session";
-import MongoDBStore from "connect-mongo";
 import mongoose from 'mongoose';
 import './db/conn.mjs'
-import { userRoutes, sessionRoutes, visitorRoutes, passwordRoutes, movieRoutes, authRoutes } from './routes/index.mjs';
+import { userRoutes, passwordRoutes, movieRoutes, authRoutes } from './routes/index.mjs';
 
 
 const PORT = process.env.PORT || 5050
@@ -20,31 +18,13 @@ app.use(cors({
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
-app.use(session({
-  name: process.env.SESS_NAME,
-  secret: process.env.SESS_SECRET,
-  resave: false,
-  saveUninitialized: false,
-  store: MongoDBStore.create({
-    client: mongoose.connection.getClient(),
-    collection: 'session',
-    ttl: parseInt(process.env.SESS_LIFETIME) / 1000
-  }),
-  cookie: {
-    sameSite: true,
-    secure: NODE_ENV === 'production',
-    maxAge: parseInt(process.env.SESS_LIFETIME)
-  }
-}));
 
 const apiRouter = express.Router();
 app.use('/api', apiRouter);
 app.use('/api', movieRoutes)
 apiRouter.use('/users', userRoutes);
 apiRouter.use('/auth', authRoutes);
-apiRouter.use('/session', sessionRoutes);
 apiRouter.use('/password', passwordRoutes);
-apiRouter.use('/', visitorRoutes);
 
 // apiRouter.use('/comment', commentRoutes)
 
