c5efbd8ef4b373cf182bd0482a9065fa55f11026
Yves Bolong <yve17@hotmail.be>
2023-12-18 12:24:21 +0100

    contactform tsx

--- client/src/components/Contact/ContactForm.tsx
+++ client/src/components/Contact/ContactForm.tsx
@@ -3,71 +3,73 @@ import './ContactForm.css'
 
 const ContactForm: React.FC = () => {
   return (
-    <form>
-      <div className="form-group">
-        <label htmlFor="firstName" className="orange-label">
-          First Name:
-        </label>
-        <input
-          type="text"
-          className="form-control"
-          id="firstName"
-          placeholder="Enter your first name"
-          required
-        />
+    <section id="contact">
+      <div className="section-content"></div>
+      <div className="contact-section">
+        <div className="container">
+          <form>
+            <div className="row">
+              <div className="col-md-6 form-line">
+                <div className="form-group">
+                  <label htmlFor="exampleInputUsername">First name</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id=""
+                    placeholder=" Enter Name"
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="exampleInputUsername">Last Name</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id=""
+                    placeholder=" Enter Name"
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="exampleInputEmail">Email Address</label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="exampleInputEmail"
+                    placeholder=" Enter Email"
+                  />
+                </div>
+
+                <div className="form-group">
+                  <label htmlFor="exampleInputUsername">Subject</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id=""
+                    placeholder=" Enter Subject"
+                  />
+                </div>
+              </div>
+              <div className="col-md-6">
+                <div className="form-group">
+                  <label htmlFor="description"> Message</label>
+                  <textarea
+                    className="form-control"
+                    id="description"
+                    placeholder="Enter Your Message"
+                    defaultValue={''}
+                  />
+                </div>
+                <div className="form-group text-center">
+                  <button type="button" className="btn btn-default submit">
+                    <i className="fa fa-paper-plane" aria-hidden="true" /> Send
+                    Message
+                  </button>
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
       </div>
-      <div className="form-group">
-        <label htmlFor="lastName" className="orange-label">
-          Last Name:
-        </label>
-        <input
-          type="text"
-          className="form-control"
-          id="lastName"
-          placeholder="Enter your last name"
-          required
-        />
-      </div>
-      <div className="form-group">
-        <label htmlFor="email" className="orange-label">
-          Email:
-        </label>
-        <input
-          type="email"
-          className="form-control"
-          id="email"
-          placeholder="Enter your email"
-          required
-        />
-      </div>
-      <div className="form-group">
-        <label htmlFor="subject" className="orange-label">
-          Subject:
-        </label>
-        <select className="form-control" id="subject">
-          <option value="General Inquiry">General Inquiry</option>
-          <option value="Technical Support">Technical Support</option>
-          <option value="Billing">Billing</option>
-          <option value="Feedback">Feedback</option>
-        </select>
-      </div>
-      <div className="form-group">
-        <label htmlFor="message" className="orange-label">
-          Message:
-        </label>
-        <textarea
-          className="form-control"
-          id="message"
-          rows={4}
-          placeholder="Enter your message"
-          required
-          defaultValue={''}
-        />
-      </div>
-      <button type="submit" className="btn btn-primary">
-        <i className="fas fa-paper-plane" /> Send
-      </button>
-    </form>
+    </section>
   )
 }
 
